c73d2347ff3a0d749e734d3a7741ad53ca889ed5
Gian Barreto <gmb.gian@gmail.com>
2020-08-14 09:35:47 -0300

    agregué más elementos en la TAS

--- app/configs/table.ts
+++ app/configs/table.ts
@@ -409,6 +409,155 @@ const table: Array<Cell> = [
             Variable.Expresion,
             Terminal.parentesisClose
         ] }, 
+    { 
+        varaible: Variable.Condicional,
+        terminal: Terminal.if, 
+        elements: [
+            Terminal.if,
+            Terminal.parentesisOpen,
+            Variable.Condicion,
+            Terminal.parentesisClose,
+            Variable.Condicion,
+            Variable.CierreCondicional
+        ] }, 
+    { 
+        varaible: Variable.CierreCondicional,
+        terminal: Terminal.puntoYComa, 
+        elements: [
+            Terminal.epsilon
+        ] }, 
+    { 
+        varaible: Variable.CierreCondicional,
+        terminal: Terminal.else, 
+        elements: [
+            Terminal.else,
+            Variable.Bloque
+        ] }, 
+    { 
+        varaible: Variable.Ciclo,
+        terminal: Terminal.while, 
+        elements: [
+            Terminal.while,
+            Terminal.parentesisOpen,
+            Variable.Condicion,
+            Terminal.parentesisClose,
+            Variable.Bloque
+        ] }, 
+    { 
+        varaible: Variable.Condicion,
+        terminal: Terminal.numero, 
+        elements: [
+            Variable.SigCondicion,
+            Variable.OpAndOr
+        ] }, 
+    { 
+        varaible: Variable.Condicion,
+        terminal: Terminal.id, 
+        elements: [
+            Variable.SigCondicion,
+            Variable.OpAndOr
+        ] }, 
+    { 
+        varaible: Variable.Condicion,
+        terminal: Terminal.menos, 
+        elements: [
+            Variable.SigCondicion,
+            Variable.OpAndOr
+        ] }, 
+    { 
+        varaible: Variable.Condicion,
+        terminal: Terminal.parentesisOpen, 
+        elements: [
+            Variable.SigCondicion,
+            Variable.OpAndOr
+        ] }, 
+    { 
+        varaible: Variable.Condicion,
+        terminal: Terminal.not, 
+        elements: [
+            Variable.SigCondicion,
+            Variable.OpAndOr
+        ] }, 
+    { 
+        varaible: Variable.Condicion,
+        terminal: Terminal.corcheteOpen, 
+        elements: [
+            Variable.SigCondicion,
+            Variable.OpAndOr
+        ] }, 
+    { 
+        varaible: Variable.OpAndOr,
+        terminal: Terminal.or, 
+        elements: [
+            Terminal.or,
+            Variable.SigCondicion,
+            Variable.OpAndOr
+        ] }, 
+    { 
+        varaible: Variable.OpAndOr,
+        terminal: Terminal.and, 
+        elements: [
+            Terminal.and,
+            Variable.SigCondicion,
+            Variable.OpAndOr
+        ] }, 
+    { 
+        varaible: Variable.OpAndOr,
+        terminal: Terminal.parentesisClose, 
+        elements: [
+            Terminal.epsilon
+        ] }, 
+    { 
+        varaible: Variable.SigCondicion,
+        terminal: Terminal.numero, 
+        elements: [
+            Variable.Expresion,
+            Variable.CierreExpresion
+        ] }, 
+    { 
+        varaible: Variable.SigCondicion,
+        terminal: Terminal.id, 
+        elements: [
+            Variable.Expresion,
+            Variable.CierreExpresion
+        ] }, 
+    { 
+        varaible: Variable.SigCondicion,
+        terminal: Terminal.menos, 
+        elements: [
+            Variable.Expresion,
+            Variable.CierreExpresion
+        ] }, 
+    { 
+        varaible: Variable.SigCondicion,
+        terminal: Terminal.parentesisOpen, 
+        elements: [
+            Variable.Expresion,
+            Variable.CierreExpresion
+        ] }, 
+    { 
+        varaible: Variable.SigCondicion,
+        terminal: Terminal.not, 
+        elements: [
+            Terminal.not,
+            Variable.SigCondicion
+        ] },
+    { 
+        varaible: Variable.SigCondicion,
+        terminal: Terminal.corcheteOpen, 
+        elements: [
+            Terminal.corcheteOpen,
+            Variable.Condicion,
+            Terminal.corcheteClose
+        ] },  
+    { 
+        varaible: Variable.CierreExpresion,
+        terminal: Terminal.signo, 
+        elements: [
+            Terminal.signo,
+            Variable.SiguienteSR,
+            Variable.Operador1
+        ] } 
 ]
 
 export default table
